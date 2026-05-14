4e55a8e4a2e076e382150bbb4c452147e689faf7
KeremOezmenHUB <kerem.oezmen@gmail.com>
2026-05-14 19:23:48 +0200

    Move frontend files to static folder and update SecurityConfig access

--- src/main/java/com/mobygo/car_rental/config/SecurityConfig.java
+++ src/main/java/com/mobygo/car_rental/config/SecurityConfig.java
@@ -26,6 +26,7 @@ public class SecurityConfig {
                         .requestMatchers("/h2-console/**").permitAll()
                         .requestMatchers("/swagger-ui.html", "/swagger-ui/**", "/v3/api-docs/**").permitAll()
                         .requestMatchers(HttpMethod.GET, "/api/cars").permitAll()
+                        .requestMatchers("/*.html", "/*.css", "/*.js").permitAll()
                         .anyRequest().authenticated()
                 )
                 .httpBasic(Customizer.withDefaults());
